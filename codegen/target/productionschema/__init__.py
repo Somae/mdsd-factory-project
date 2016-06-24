4a939cb57396e05d15efc984081216b4ce1d1771
lukasjf <lukas.faber@student.hpi.de>
2016-06-24 22:35:02 +0200

    add generation for packages

---
+++ codegen/target/productionschema/__init__.py
@@ -0,0 +1,21 @@
+from ProductionSchema import ProductionSchema
+
+
+from ConjunctiveNode import ConjunctiveNode
+
+
+from DisjunctiveNode import DisjunctiveNode
+
+
+from MaterialLink import MaterialLink
+
+
+from LinkableNode import LinkableNode
+
+
+from IdentifiableElement import IdentifiableElement
+
+
+from Material import Material
+
+
