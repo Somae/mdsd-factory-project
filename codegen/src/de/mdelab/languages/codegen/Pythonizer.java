6b53cac27a92ad6113d86ca9347a3a94d39956df
lukasjf <lukas.faber@student.hpi.de>
2016-07-14 20:10:29 +0200

    added pythonization for names of constants

--- codegen/src/de/mdelab/languages/codegen/Pythonizer.java
+++ codegen/src/de/mdelab/languages/codegen/Pythonizer.java
@@ -15,4 +15,18 @@ public class Pythonizer {
 		}
 		return builder.toString();
 	}
+	
+	public String pythonizeConstant(String name){
+		StringBuilder builder = new StringBuilder();
+		
+		for (int i = 0; i < name.length(); i++){
+			if( i != 0 && Character.isUpperCase(name.charAt(i))){
+				builder.append('_');
+				builder.append(name.charAt(i));
+			} else{
+				builder.append(Character.toUpperCase(name.charAt(i)));
+			}
+		}
+		return builder.toString();
+	}
 }
